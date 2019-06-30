cea0be1709821f973f1b3f6338ba16c0a242a244
LukaRoguljic <luka.roguljic16@gmail.com>
2019-06-30 19:20:11 +0200

    Adding markers manually with city name of tournament added and new table in SQLIte db

--- app/src/main/java/com/example/csgorganizer/DatabaseHelper.java
+++ app/src/main/java/com/example/csgorganizer/DatabaseHelper.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -13,25 +14,29 @@ import java.util.List;
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "RMA.db";
     public static final String TABLE_NAME = "practices";
+    public static final String TABLE_CITY_NAME = "cities";
     public static final String COL_1 = "ID";
     public static final String COL_2 = "MAPS";
     public static final String COL_3 = "DATE";
     public static final String COL_4 = "COMMENT";
     public static final String COL_5 = "OPPONENT";
     public static final String COL_6 = "CREATED";
+    public static final String COL_CITY_NAME = "CITYNAME";
 
     public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, 1);
+        super(context, DATABASE_NAME, null, 2);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table " + TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT, MAPS TEXT, DATE TEXT, COMMENT TEXT, OPPONENT TEXT, CREATED INTEGER)");
+        db.execSQL("create table " + TABLE_CITY_NAME +" (ID_CITY INTEGER PRIMARY KEY AUTOINCREMENT, CITYNAME TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CITY_NAME);
         onCreate(db);
     }
 
@@ -53,6 +58,26 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         else
             return true;
     }
+
+    public boolean insertCityNames(String name){
+        if(name.isEmpty()){
+            SQLiteDatabase db = this.getWritableDatabase();
+            Log.d("this is my array", "PRAZAN UNOOOOOOOOOOS");
+            return false;
+        }
+        else{
+            SQLiteDatabase db = this.getWritableDatabase();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(COL_CITY_NAME, name);
+            long result = db.insert(TABLE_CITY_NAME, null, contentValues);
+            if(result == -1) {
+                return false;
+            }
+            else
+                return true;
+        }
+    }
+
 
     public int deleteData(){
         SQLiteDatabase db = this.getWritableDatabase();
@@ -82,6 +107,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             arrayList.add(listItem);
         }
         return arrayList;
+    }
+
+    public String[] getLocationNames(){
+        String[] locationNames = new String[10];
+        int i = 0;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM cities WHERE CITYNAME IS NOT NULL", null);
+        while(cursor.moveToNext()){
+            locationNames[i] = cursor.getString(1);
+            i = i + 1;
+        }
+        return locationNames;
     }
 
 }
--- app/src/main/java/com/example/csgorganizer/MapActivity.java
+++ app/src/main/java/com/example/csgorganizer/MapActivity.java
@@ -1,8 +1,18 @@
 package com.example.csgorganizer;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,9 +21,19 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     GoogleMap map;
+    DatabaseHelper myDB;
+    EditText etAddCity;
+    Button btnAddCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +42,30 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
         SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        myDB = new DatabaseHelper(this);
+        etAddCity = (EditText)findViewById(R.id.newLocName);
+        btnAddCity = (Button)findViewById(R.id.btnAddLocation);
+
+        AddCityName();
+    }
+
+    public void  AddCityName(){
+        btnAddCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               boolean isInserted =  myDB.insertCityNames(etAddCity.getText().toString());
+
+                if(isInserted){
+                    Toast.makeText(MapActivity.this,"Location added", Toast.LENGTH_LONG).show();
+                }
+                if(!isInserted){
+                    Toast.makeText(MapActivity.this,"FAILED", Toast.LENGTH_LONG).show();
+                }
+
+                Intent addCityIntent = new Intent(MapActivity.this, MapActivity.class);
+                startActivity(addCityIntent);
+            }
+        });
     }
 
     @Override
@@ -29,8 +73,26 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
         map = googleMap;
 
-        LatLng odense = new LatLng(55.396710, 10.383740);
-        map.addMarker(new MarkerOptions().position(odense).title("MAJOR"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(odense));
+        String[] nameArray = myDB.getLocationNames();
+        Geocoder obje = new Geocoder(this);
+        double latitude = 0,longtitude = 0;
+        int i = 0;
+
+        for (i = 0; i < 10; i++){
+            if(nameArray[i] == null){}
+            else{
+                try{
+                    List<Address> coordinates = obje.getFromLocationName(nameArray[i], 1);
+                    if (coordinates.size() >  0) {
+                        latitude = coordinates.get(0).getLatitude();
+                        longtitude = coordinates.get(0).getLongitude();
+                        LatLng newLocation = new LatLng(latitude, longtitude);
+                        map.addMarker(new MarkerOptions().position(newLocation).title("CSGO"));
+                    }
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
