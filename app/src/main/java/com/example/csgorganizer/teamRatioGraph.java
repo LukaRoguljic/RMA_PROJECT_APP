8fb6ec1ae29c862bbf468d10f320fe87aecf15eb
LukaRoguljic <luka.roguljic16@gmail.com>
2019-07-05 17:03:37 +0200

    Kreiran scroll view u grafici , dodana dva nova grafa. Posljednji commit

--- app/src/main/java/com/example/csgorganizer/teamRatioGraph.java
+++ app/src/main/java/com/example/csgorganizer/teamRatioGraph.java
@@ -5,13 +5,17 @@ import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -20,6 +24,7 @@ public class teamRatioGraph extends AppCompatActivity {
 
     BarChart barChart;
     LineChart lineChart;
+    PieChart weaponChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +33,7 @@ public class teamRatioGraph extends AppCompatActivity {
 
         barChart    = (BarChart)findViewById(R.id.barGraph);
         lineChart   = (LineChart)findViewById(R.id.mapPercentage);
+        weaponChart = (PieChart)findViewById(R.id.pieChartWeapon);
 
         ArrayList<BarEntry> barEntries = new ArrayList<>();
 
@@ -37,7 +43,7 @@ public class teamRatioGraph extends AppCompatActivity {
         barEntries.add(new BarEntry(0.88f,3));
         barEntries.add(new BarEntry(0.99f,4));
 
-        BarDataSet barDataSet = new BarDataSet(barEntries, "Values");
+        BarDataSet barDataSet = new BarDataSet(barEntries, "");
 
         ArrayList<String> theNames = new ArrayList<>();
         theNames.add("Guard");
@@ -51,6 +57,8 @@ public class teamRatioGraph extends AppCompatActivity {
         barChart.setTouchEnabled(true);
         barChart.setDragEnabled(true);
         barChart.setScaleEnabled(true);
+        barChart.setDescription("KDA RATIO");
+
 
         ArrayList<Entry> lineEntries = new ArrayList<>();
 
@@ -62,7 +70,7 @@ public class teamRatioGraph extends AppCompatActivity {
         lineEntries.add(new Entry(54.8f, 5));
         lineEntries.add(new Entry(53.9f, 6));
 
-        LineDataSet lineDataSet = new LineDataSet(lineEntries, "Values");
+        LineDataSet lineDataSet = new LineDataSet(lineEntries, "");
         ArrayList<String> mapNames = new ArrayList<>();
         mapNames.add("Overpass");
         mapNames.add("Mirage");
@@ -78,8 +86,33 @@ public class teamRatioGraph extends AppCompatActivity {
         lineChart.setTouchEnabled(true);
         lineChart.setDragEnabled(true);
         lineChart.setScaleEnabled(true);
+        lineChart.setDescription("");
 
 
+        ArrayList<Entry> pieEntries = new ArrayList<>();
+
+
+        pieEntries.add(new Entry(40.5f, 0));
+        pieEntries.add(new Entry(16.15f, 1));
+        pieEntries.add(new Entry(15.91f, 2));
+        pieEntries.add(new Entry(10.69f, 3));
+        pieEntries.add(new Entry(16.75f, 4));
+
+        PieDataSet pieDataSet = new PieDataSet(pieEntries, "");
+
+        ArrayList<String> weaponNames = new ArrayList<>();
+        weaponNames.add("AK-47");
+        weaponNames.add("AWP");
+        weaponNames.add("M4A1");
+        weaponNames.add("AUG");
+        weaponNames.add("Rest");
+
+        PieData theData3 = new PieData(weaponNames, pieDataSet);
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        weaponChart.setData(theData3);
+        weaponChart.setTouchEnabled(true);
+        weaponChart.setDescription("");
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
 
 
     }
